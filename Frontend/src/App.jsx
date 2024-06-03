914ba63b02eb456d270d7f1f2376761bf0f4b702
Fathi Abdi <fathiabdi254@gmail.com>
2024-06-03 20:43:38 +0300

    Added the header section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,11 +6,13 @@ import Signup from './Pages/Signup'
 import Signin from './Pages/Signin'
 import Dashboard from './Pages/Dashboard'
 import Projects from './Pages/Projects'
+import Header from './components/header'
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
